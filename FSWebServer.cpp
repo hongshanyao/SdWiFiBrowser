19f67005b06c8d7e8c7eb51f86f596372291e33f
hongshanyao <hongshanyao@outlook.com>
2025-03-09 14:55:44 +0800

    Update password input style

--- FSWebServer.cpp
+++ FSWebServer.cpp
@@ -107,6 +107,7 @@ String getContentType(String filename, AsyncWebServerRequest *request) {
   else if (filename.endsWith(".gif")) return "image/gif";
   else if (filename.endsWith(".jpg")) return "image/jpeg";
   else if (filename.endsWith(".ico")) return "image/x-icon";
+  else if (filename.endsWith(".svg")) return "image/svg+xml";
   else if (filename.endsWith(".xml")) return "text/xml";
   else if (filename.endsWith(".pdf")) return "application/x-pdf";
   else if (filename.endsWith(".zip")) return "application/x-zip";
